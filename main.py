b2ec470182415347750dcfab8609b03088a86605
Diego <90355570+PanduCa@users.noreply.github.com>
2024-11-09 21:27:36 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Import
-from flask import Flask, render_template,request, redirect
+from flask import Flask, render_template, request 
 
 
 
@@ -17,6 +17,24 @@ def process_form():
     button_python = request.form.get('button_python')
     return render_template('index.html', button_python=button_python)
 
+
+#FORMULARIO
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    
+    email = request.form['email']
+    comment = request.form['comment']
+
+    with open('form.txt', 'a',) as f:
+        f.write(f"Correo electrónico: {email} \n")
+        f.write(f"Fecha: {comment} \n")
+            
+    
+    return render_template('form_result.html', 
+                           
+                           email=email,
+                           comment=comment,
+                           )
 
 if __name__ == "__main__":
     app.run(debug=True)
